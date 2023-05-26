128ee135b1c90bb16d14f8299a63fadce9ba1574
Elias Hezron <eliashezron@Eliass-MacBook-Pro.local>
2023-05-26 18:13:00 +0300

    commit

---
+++ src/ERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "solmate/tokens/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("Token", "TKN", 18) {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
